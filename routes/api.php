ad5db6e52734bcd200523711575c2cde6a5fd42b
Allan Milani <allan.milani582@gmail.com>
2022-12-25 09:34:57 -0300

    feat: :rocket: Adicionando cadastramento de chaves do google

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/savedKeyGoogle', [UserController::class, 'savedKeyGoogle']);
 });
